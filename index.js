d0c8fa803bd59f80d028c12d96a54d7ed11baa95
guavalines <konstrictor@gmail.com>
2022-12-17 22:44:37 -0800

    added js

---
+++ index.js
@@ -0,0 +1,4 @@
+const testimonial = {
+  name: "Winifred K.",
+  photoUrl: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+}
