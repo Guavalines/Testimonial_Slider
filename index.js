18ddde5d3eff7b158013854305293d1a8124c64a
guavalines <konstrictor@gmail.com>
2022-12-17 22:47:55 -0800

    added js text

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const testimonial = {
   name: "Winifred K.",
-  photoUrl: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+  photoUrl: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+  text: "Thanks to Apple, we've just launched our 5th website! Apple saved my business. Man, this thing is getting better and better as I learn more about it. I would gladly pay over 600 dollars for Apple."
 }
